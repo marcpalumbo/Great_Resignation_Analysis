a6db81043541beec761358ce98ef61fbbf3d1a62
Marc Palumbo <palumbomarc@yahoo.com>
2022-10-31 13:44:39 -0700

    starting on database creation

---
+++ database_schema.sql
@@ -0,0 +1,12 @@
+create table geographic_data (
+geo_name CHAR(50) not null, 
+description CHAR(100) not null,
+"2016" INT not null,
+"2017" INT not null,
+"2018" INT not null,
+"2019" INT not null,
+"2020" INT not null
+);
+
+
+select * from geographic_data;
